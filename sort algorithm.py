0b9a14f1d1bdf3254a469b3a4dd0523166a8a20d
samminmul <74859160+samminmul@users.noreply.github.com>
2024-04-08 09:39:50 +0900

    버블정렬까지 구현

--- sort algorithm.py
+++ sort algorithm.py
@@ -1,4 +1,10 @@
-#선택, 삽입, 버블, 병합, 퀵, 힙
+#선택, 삽입, 버블, 병합, 퀵, 힙, 카운팅, 인트로
+
+
+def swap(arr, i, j, check):
+    arr[i], arr[j] = arr[j], arr[i]
+    if check:
+        print(f"swap {i}, {j} -> {arr}")
 
 def selection(arr, check=False):
     if check: print("---selcection sort---")
@@ -8,19 +14,31 @@ def selection(arr, check=False):
         for j in range(i + 1, len(arr)):
             if arr[min_idx] > arr[j]:
                 min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        swap(arr, i, min_idx, check)
         
-        if check: print(f"swap {i}, {min_idx}: {arr}")
-        
-    if check: print("---selcection sort end---")
     return arr
 
 def insertion(arr, check=False):
     if check: print("---insetion sort---")
 
-    for i in range(len(arr)):
+    for i in range(1, len(arr)):
+        while arr[i - 1] > arr[i] and i > 1:
+            swap(arr, i, i - 1, check)
+            i -= 1
         
+    return arr
 
-arr = [1, 5, 3, 2, 4]
-sorted_arr = selection(arr, check=True)
+def bubble(arr, check=False):
+    if check: print("---bubble sort---")
+    
+    for _ in range(len(arr)):
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                swap(arr, i, i + 1, check)
+
+    return arr
+
+            
+arr = [5, 2, 4, 3, 1]
+sorted_arr = bubble(arr, check=True)
 print(sorted_arr)

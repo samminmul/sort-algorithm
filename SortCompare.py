da83b88cc1cfd8a1196c5a6ef7a158b84e5a5819
samminmul <74859160+samminmul@users.noreply.github.com>
2024-05-12 01:59:51 +0900

    랜덤배열 그래프를 그렸다!

--- SortCompare.py
+++ SortCompare.py
@@ -9,7 +9,7 @@ sorts = sort_collection[:-1] #counting_sort2는 일단 제외
 sort_times = [[0]*10 for _ in range(len(sorts))]
 
 lengths_of_arrs_to_sort = [i*1000 for i in range(1, len(sort_times[0]) + 1)]
-REPEAT = 1
+REPEAT = 5
 for _ in range(REPEAT):
     for i in range(len(lengths_of_arrs_to_sort)):
         arr = [randrange(100) for _ in range(lengths_of_arrs_to_sort[i])]
@@ -22,6 +22,7 @@ for _ in range(REPEAT):
 
             print(lengths_of_arrs_to_sort[i], sort_time, sorts[j].__name__, sep=" ; ")
             sort_times[j][i] += sort_time/REPEAT
+
 
 for i in range(len(sorts)):
     plt.plot(lengths_of_arrs_to_sort, sort_times[i], label=sorts[i].__name__)
